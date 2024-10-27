851e751dcc9233218b3826fc8c0c17ef6c83e01e
Cristian <20240250@leon.tecnm.mx>
2024-10-26 21:48:30 -0600

    Bash

--- sample-app.sh
+++ sample-app.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-# apt-get update
-# apt-get install docker-compose-plugin
-
-# mkdir -p ~/.docker/cli-plugins/
-# curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-# chmod +x ~/.docker/cli-plugins/docker-compose
-
-
-# Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-docker run hello-world
-
-# docker compose version
-
-# docker compose up
+docker compose up
