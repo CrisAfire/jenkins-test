43cd21c3bd3d468d666ba591739c1d6242d18d17
Cristian <20240250@leon.tecnm.mx>
2024-10-26 19:44:56 -0600

    Bash

--- sample-app.sh
+++ sample-app.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker --version
+
+# apt-get update
+# apt-get install docker-compose-plugin
+
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
+docker compose up
